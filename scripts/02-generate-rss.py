7f8c7864ed6ca3696f482845a9a3c3dd31782d93
Jeremy Singer-Vine <jsvine@gmail.com>
2023-03-29 18:04:55 -0400

    Refactor and flesh out RSS generation
    - Adds content/details to the feed and entries.
    - Adds two command-line options to control how far back (in terms of
      incident date) to look, and the threshold (in terms of days since
      discovery) for inclusion in the RSS feed(s).
    - Removes hashing/etc., which was a holdover from a prior script and not
      needed here.
    - Switches to pd.DataFrame.merge to join the reports to their discovery
      dates.
    - Autoformats code with isort and black; adds type hinting to appease
      mypy.

--- scripts/02-generate-rss.py
+++ scripts/02-generate-rss.py
@@ -1,111 +1,129 @@
 import argparse
-import hashlib
-import os
+import html
 import re
 import sys
+import typing
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
 
 import pandas as pd
-
-from datetime import datetime, timezone
 from feedgen.feed import FeedGenerator
-from glob import glob
-
 
 BASE_ID = "data-liberation-project:phma-hazmat-incident-reports"
-HASH_SALT = "x#$+w3%~o;0~V+e'"
 RE_PATTERN = r"^(?:<a href = .*?>)?([A-Z]+-[0-9]+)(?:</A>)?$"
+NOW = datetime.now(tz=timezone.utc)
 
 
-def parse_args():
+def parse_args(args: list[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_path")
-    return parser.parse_args()
+    parser.add_argument(
+        "--num-months",
+        type=int,
+        default=12,
+        help="The number of months (by incident date) to consider for inclusion. Defaults to 12.",  # noqa: E501
+    )
+    parser.add_argument(
+        "--discovered-days",
+        type=int,
+        default=7,
+        help="In RSS feeds, show only reports *discovered* in past X days. Defaults to 7.",  # noqa: E501
+    )
+    return parser.parse_args(args)
 
 
-def convert_entry(entry_row):
-    hash_payload = f"{entry_row['report_number']}:{HASH_SALT}".encode("utf-8")
-    new_id = hashlib.sha1(hash_payload).hexdigest()
-    re_link = re.match("^<a href = (.+)>([A-Z]-[0-9]+)</A>$",entry_row["Report Number"])
-    if re_link:
-        if len(re_link.groups()) < 2:
-            breakpoint()
-        link = re_link.group(1)
-    else:
-        link = None
-    title = entry_row['report_number']
+def convert_entry(row: dict[str, typing.Any]) -> dict[str, typing.Any]:
+    report_number = row["report_number"]
+    report_link_match = re.search("href = (.+)>", row["Report Number"])
+
+    link = report_link_match.group(1) if report_link_match else None
+
     return dict(
-        id=BASE_ID + ":" + new_id,
-        link={"link":{"href":link}},
-        title=title,
-        published=entry_row['timestamp'],
+        id=BASE_ID + ":" + report_number,
+        link={"link": {"href": link}},
+        title=f"Report {report_number}",
+        published=row["timestamp"],
         content=dict(
-            content=f'<a link="{link}">{title}</a>',
+            content=f"""
+            <h3><a link="{link}">Report {report_number}</a></h3>
+            <h4>Identified by scraper @ {row["timestamp"]}</h4>
+            <ul>
+                <li>Incident Date: {row["Date Of Incident"]}</li>
+                <li>City: {row["Incident City"]}</li>
+                <li>State: {row["Incident State"]}</li>
+                <li>Report Type: {row["Report Type"]}</li>
+                <li>Mode Of Transportation: {row["Mode Of Transportation"]}</li>
+                <li>Carrier/Reporter: {row["Carrier Reporter Name"]}</li>
+                <li>Hazmat Fatalities: {row["Total Hazmat Fatalities"]}</li>
+                <li>Hazmat Injuries: {row["Total Hazmat Injuries"]}</li>
+                <li>Non-Hazmat Fatalities: {row["Non Hazmat Fatalities"]}</li>
+                <li>Description: {html.escape(row["Description Of Events"])}</li>
+            </ul>
+            """,
             type="html",
         ),
     )
 
-def convert_feed(entry_data):
-    #TODO: Fill these values with something that makes sense. Not sure what "updated" is without an actual parent feed.
+
+def convert_feed(rows: pd.DataFrame) -> FeedGenerator:
     feed_attrs = {
-        "title": "Lorem Ipsum",
+        "title": "Latest PHMSA Hazardous Materials Incident Reports",
         "id": BASE_ID,
-        "subtitle": "Dolor Something Something: Please Change This!",
+        "subtitle": "The latest Form 5800.1 hazardous material reports submitted to the Department of Transportation and posted online by the Pipeline and Hazardous Materials Safety Administration.",  # noqa: E501
         "author": {"name": "Data Liberation Project"},
         "language": "en",
         "link": {
-            "href": "https://github.com/data-liberation-project/phma-hazmat-incident-reports",
+            "href": "https://github.com/data-liberation-project/phma-hazmat-incident-reports",  # noqa: E501
         },
-        "updated": datetime.now(tz=timezone.utc), #?
+        "updated": NOW.isoformat(),
     }
 
     fg = FeedGenerator()
     for k, v in feed_attrs.items():
         getattr(fg, k)(v)
 
-    for _, row in entry_data.iterrows():
-        if isinstance(row['Report Number'],str):
-            e = convert_entry(row)
-            new_entry = fg.add_entry()
-            for k, v in e.items():
-                method = getattr(new_entry, k)
-                if isinstance(v, dict):
-                    method(**v)
-                else:
-                    method(v)
+    for _, row in rows.iterrows():
+        e = convert_entry(row)
+        new_entry = fg.add_entry()
+        for k, v in e.items():
+            method = getattr(new_entry, k)
+            if isinstance(v, dict):
+                method(**v)
+            else:
+                method(v)
 
     return fg
 
 
-def fetch_entry_data(data_path):
-    #TODO: structure this differently to filter by date
-    discovered_date_files = glob(os.path.join(data_path,r'processed/discovered-dates/*.csv'))
-    fetched_data_files = [os.path.join(data_path,'fetched',os.path.basename(ddf)) for ddf in discovered_date_files]
-    discovered_dates = pd.DataFrame()
-    for ddf in discovered_date_files:
-        discovered_dates = pd.concat(
-            [
-                discovered_dates,
-                pd.read_csv(ddf)
-            ],
-            ignore_index=True
+def fetch_entry_data(num_months: int, discovered_days: int) -> pd.DataFrame:
+    hist_paths = Path("data/processed/discovered-dates").glob("*.csv")
+    hist_paths_limited = sorted(hist_paths)[-num_months:]
+    data_paths = Path("data/fetched").glob("*.csv")
+    data_paths_limited = sorted(data_paths)[-num_months:]
+
+    history = pd.concat(map(pd.read_csv, hist_paths_limited))
+    data = (
+        pd.concat(map(pd.read_csv, data_paths_limited))
+        .drop_duplicates(subset=["Report Number"])
+        .assign(
+            report_number=lambda df: df["Report Number"].str.extract(
+                RE_PATTERN, expand=False
+            )
         )
-    discovered_dates = discovered_dates.sort_values('timestamp')
-    entry_data = pd.DataFrame()
-    for fdf in fetched_data_files:
-        entry_data = pd.concat(
-            [
-                entry_data,
-                pd.read_csv(fdf)
-            ]
-        )
-    entry_data['report_number'] = [re.match(RE_PATTERN,rn).group(1) for rn in entry_data['Report Number']]
-    entry_data = pd.merge(entry_data, discovered_dates, how='outer', on='report_number')
-    return entry_data
+    )
+
+    rows = history.loc[
+        lambda df: pd.to_datetime(df["timestamp"])
+        > (NOW - timedelta(days=discovered_days))
+    ].merge(data, on=["report_number"], validate="1:1")
+
+    return rows
 
 
-def main():
-    args = parse_args()
-    entry_data = fetch_entry_data(args.data_path)
+def main() -> None:
+    args = parse_args(sys.argv[1:])
+    entry_data = fetch_entry_data(
+        num_months=args.num_months, discovered_days=args.discovered_days
+    )
     converted = convert_feed(entry_data)
     converted.rss_file(sys.stdout.buffer, pretty=True)
 
