c38e28f953002d4d4670984911f061613c71f2a0
Jeremy Singer-Vine <jsvine@gmail.com>
2023-04-02 16:37:07 -0400

    Auto-extract the fmapid instead of hardcoding

--- scripts/lib/scraper.py
+++ scripts/lib/scraper.py
@@ -124,13 +124,21 @@ class Session:
 
         self.get("init", init_url, headers=HEADERS)
         self.get("portal", portal_url, headers=custom_headers, allow_redirects=True)
+
         self.post(
             "auth",
             auth_url,
             data="---------------------------------------------------------",
             headers=HEADERS,
         )
-        self.get("portal", portal_url, headers=HEADERS, allow_redirects=True)
+        res = self.get("portal", portal_url, headers=HEADERS, allow_redirects=True)
+
+        fmap_match = re.search('fmapId=([^"]+)"', res.text)
+
+        if fmap_match is None:
+            raise ValueError("Cannot find fmapId in initialization step")
+
+        self.fmapid = fmap_match.group(1)
 
     def compose_query(self, **params: str) -> str:
         query_xml = CLIENT_QUERY_XML
@@ -152,7 +160,7 @@ class Session:
         data = dict(
             ViewState="7m3hnikntaqp81992oti7ajp52",
             ClientStateXml=CLIENT_STATE_XML,
-            fmapId="1CPXXw",
+            fmapId=self.fmapid,
             reloadTargets="all",
             Page="Hazmat Incident Report",
             IgnoreBypassCacheOption="ignoreBypassCache",
